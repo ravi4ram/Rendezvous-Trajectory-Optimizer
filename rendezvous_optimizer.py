da5fc7a357dee9fa52508e95863ff404a6617b4c
ravi4ram <ravindran_ram@yahoo.com>
2025-01-10 08:35:06 +0530

    plot function fix

--- rendezvous_optimizer.py
+++ rendezvous_optimizer.py
@@ -99,11 +99,10 @@ def estimate():
     prob.solve(solver=cp.CLARABEL)
     
     # on success plot the result
-    if prob.status == 'infeasible' :
-        print(f'STATUS : {prob.status}')
-        return
+    if prob.status == 'optimal' :
+        plot(x.value, u.value)        
     else:
-        plot(x.value, u.value)
+        print(f'STATUS : {prob.status}')
     
     # return
     return
@@ -114,7 +113,9 @@ def estimate():
 # target satellite and the k^ direction pointing in the crosstrack direction,
 # out of the orbital plane, to complete the orthogonal set.
 def plot(X, U):
-    X = X/1E3 # km conversion for display
+    # km conversion for display
+    X = np.round(X, 2)/1E3
+    
     x, y, z    = X[2, :], X[1, :], X[0, :]
     xi, yi, zi = X[2, 0], X[1, 0], X[0, 0]
     xf, yf, zf = X[2, -1], X[1, -1], X[0, -1]
@@ -122,40 +123,63 @@ def plot(X, U):
     #U = control data
     ux, uy, uz = U[2, :].T, U[1, :].T, U[0, :].T
     
+    # Set 3D plot axes to equal scale. 
+    # Required since `ax.axis('equal')` and `ax.set_aspect('equal')` don't work on 3D.
+    # https://stackoverflow.com/questions/13685386/matplotlib-equal-unit-length-with-equal-aspect-ratio-z-axis-is-not-equal-to
+    def set_axes_equal_3d(ax: plt.Axes):    
+        limits = np.array([
+            ax.get_xlim3d(),
+            ax.get_ylim3d(),
+            ax.get_zlim3d(),
+        ])
+        origin = np.mean(limits, axis=1)
+        radius = 0.5 * np.max(np.abs(limits[:, 1] - limits[:, 0]))
+        # set axes
+        x, y, z = origin
+        ax.set_xlim3d([x - radius, x + radius])
+        ax.set_ylim3d([y - radius, y + radius])
+        ax.set_zlim3d([z - radius, z + radius])
+        #
+        return
+    
     # setup plot canvas
-    fig = plt.figure(figsize=(9, 5), constrained_layout=True)
+    fig = plt.figure(figsize=(8, 5), constrained_layout=True)
     fig.suptitle('Rendezvous and docking Trajectory Optimization', fontsize=10)
     gs = fig.add_gridspec(3,3)
 
     # 1st col
     ax1 = fig.add_subplot(gs[0, 0])
     ax1.plot(ux, '-b', linewidth=0.8)
-    ax1.set_title('Control History', fontsize=9)
-    ax1.set_ylabel(r'Fx ($m/s^2$)', fontsize=9)
+    ax1.set_title('Control History', fontsize=7)
+    ax1.set_ylabel(r'Fx ($m/s^2$)', fontsize=7)
     plt.setp(ax1.get_xticklabels(), visible=False)
 
     ax2 = fig.add_subplot(gs[1, 0], sharex=ax1)
     ax2.plot(uy, '-b', linewidth=0.8)
-    ax2.set_ylabel(r'Fy ($m/s^2$)', fontsize=9)
+    ax2.set_ylabel(r'Fy ($m/s^2$)', fontsize=7)
     plt.setp(ax2.get_xticklabels(), visible=False)
 
     ax3 = fig.add_subplot(gs[2, 0], sharex=ax1)
     ax3.plot(uz, '-b', linewidth=0.8)   
-    ax3.set_ylabel(r'Fz ($m/s^2$)', fontsize=9)
+    ax3.set_ylabel(r'Fz ($m/s^2$)', fontsize=7)
     ax3.set_xlabel('Timestep', fontsize=9)
 
     # 2nd col - 3-rows
     ax4 = fig.add_subplot(gs[:, 1:3], projection='3d') 
     ax4.view_init(azim=40., elev=12.)
     ax4.plot(x, y, z, '-b', linewidth=1.5)
-    ax4.scatter(xi, yi, zi, color='green') # , s=100
-    ax4.scatter(xf, yf, zf, color='red') # , s=100    
+    ax4.scatter(xi, yi, zi, color='green')
+    ax4.scatter(xf, yf, zf, color='red')    
     ax4.set_title('Chaser Trajectory', fontsize=7)
     ax4.set(xlabel='Z-Out of Plane (km)',
             ylabel='Y-Along Track (km)',
             zlabel='X-Radial (km)')
     ax4.legend(['Trajectory', 'Start Point', 'End Point'], loc='upper center',
-              bbox_to_anchor=(0.5, -0.1), frameon=True, borderaxespad=0, ncol = 3)    
+              bbox_to_anchor=(0.5, -0.1), frameon=True, borderaxespad=0, ncol = 3)
+
+    # set correct aspect ratio
+    ax4.set_box_aspect([1,1,1])
+    set_axes_equal_3d(ax4)    
 
     # set ticks and grid lines
     for ax in [ax1, ax2, ax3]:
@@ -166,8 +190,8 @@ def plot(X, U):
         ax.minorticks_on()
         ax.xaxis.set_tick_params(labelsize=7, rotation=90)
         ax.yaxis.set_tick_params(labelsize=7)    
-    # end-for        
-
+    # end-for
+    
     plt.show()
     # return
     return
